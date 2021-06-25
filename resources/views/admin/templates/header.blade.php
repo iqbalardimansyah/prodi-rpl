305340c9dbcda3caed82b63e03b66178c6a7cfa8
unknown <farhan.ff.fauzaan@gmail.com>
2021-06-26 06:37:59 +0700

    bug

--- resources/views/admin/templates/header.blade.php
+++ resources/views/admin/templates/header.blade.php
@@ -39,8 +39,8 @@
         <!-- Top bar area -->
         <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                <a class="navbar-brand brand-logo mr-5" href="index-2.html"><img src="{{asset('assets')}}/admin/img/core-img/logo.png" class="mr-2" alt="logo" /></a>
-                <a class="navbar-brand brand-logo-mini" href="index-2.html"><img src="{{asset('assets')}}/admin/img/core-img/small-logo.png" alt="logo" /></a>
+                <a class="navbar-brand brand-logo mr-5" href="{{url('/admin')}}"><img src="{{asset('assets')}}/admin/img/core-img/logo.png" class="mr-2" alt="logo" /></a>
+                <a class="navbar-brand brand-logo-mini" href="{{url('/admin')}}"><img src="{{asset('assets')}}/admin/img/core-img/small-logo.png" alt="logo" /></a>
             </div>
             <div class="navbar-menu-wrapper d-flex align-items-center justify-content-between">
                 <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
